782bde6cbd2b2aa19cba6fb88f2860327a8d91ec
lleung030 <lucasleung2593@gmail.com>
2023-08-16 13:55:34 -0700

    fetchCars, homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,18 @@
-// import { fetchCars } from "@utils";
-// import { HomeProps } from "@types";
+import { fetchCars } from "@/utils";
+import { HomeProps } from "@/types";
 import { fuels, yearsOfProduction } from "@/constants";
 import { CarCard, ShowMore, SearchBar, CustomFilter, Hero } from "@/components";
 
 export default async function Home({ searchParams }: HomeProps) {
-  // const allCars = await fetchCars({
-  //   manufacturer: searchParams.manufacturer || "",
-  //   year: searchParams.year || 2022,
-  //   fuel: searchParams.fuel || "",
-  //   limit: searchParams.limit || 10,
-  //   model: searchParams.model || "",
-  // });
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
 
-  // const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className='overflow-hidden'>
@@ -33,7 +33,7 @@ export default async function Home({ searchParams }: HomeProps) {
           </div>
         </div>
 
-        {/* {!isDataEmpty ? (
+        {!isDataEmpty ? (
           <section>
             <div className='home__cars-wrapper'>
               {allCars?.map((car) => (
@@ -51,7 +51,7 @@ export default async function Home({ searchParams }: HomeProps) {
             <h2 className='text-black text-xl font-bold'>Oops, no results</h2>
             <p>{allCars?.message}</p>
           </div>
-        )} */}
+        )}
       </div>
     </main>
   );
--- components/index.ts
+++ components/index.ts
@@ -1,16 +1,19 @@
-import Hero from './Hero'
-import CustomButton from './CustomButton'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import SearchBar from './SearchBar'
-import CustomFilter from './CustomFilter'
-
+import CarCard from "./CarCard";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import NavBar from "./Navbar";
+import ShowMore from "./ShowMore";
+import SearchBar from "./SearchBar";
+import Hero from "./Hero";
 
 export {
-    Hero,
-    CustomButton,
-    Navbar,
-    Footer,
-    SearchBar,
-    CustomFilter,
-}
+  Hero,
+  CarCard,
+  CustomButton,
+  CustomFilter,
+  Footer,
+  NavBar,
+  ShowMore,
+  SearchBar,
+};
