e4f51d25da05e00b5f0a5ac896c2b18313ca9633
lleung030 <lucasleung2593@gmail.com>
2023-08-16 11:05:23 -0700

    custom button hero page

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,11 @@
+'use client'
 import React from 'react'
+import CustomButton from './CustomButton'
 
 function Hero() {
+    const handleScroll = () => {
+
+    }
   return (
     <div className='hero'>
       <div className='flex-1 pt-36 padding-x'>
@@ -11,6 +16,13 @@ function Hero() {
         <p className='hero__subtitle'>
             Streamline your car rental experience with our effortless booking process.
         </p>
+
+        <CustomButton 
+            title='Explore Cars'
+            containerStyles='bg-primary-blue
+            text-white rounded-full mt-10'
+            handleClick={handleScroll}
+        />
       </div>
     </div>
   )
