480450e3c2a45441a7fd6a899b8e62261649fb6a
lleung030 <lucasleung2593@gmail.com>
2023-08-16 11:25:39 -0700

    hero image

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -27,7 +27,9 @@ function Hero() {
       </div>
       <div className='hero__image-container'>
          <div className='hero__image'>
-            <Image src='' alt={''}/>
+            <Image src='/hero.png' alt='hero'
+            fill className='object-contain'
+            />
          </div>
       </div>
     </div>
